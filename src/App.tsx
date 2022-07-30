e95d39bb6acdfeae67774dc301d8574b8d3f86e3
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2022-07-31 00:16:51 +0530

    chore: initial 10s timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import { motion } from 'framer-motion'
+import TimeComponent from './components/TimeComponent'
 
 function App() {
   return (
-    <div className="flex h-screen">
-      <div className="m-auto">
-        <motion.div
-          animate={{
-            y: [20, -20, 20],
-          }}
-          transition={{ duration: 1, repeat: Infinity }}
-        >
-          <h3 className="font-bold font-mono">Pomodora is being built!</h3>
-        </motion.div>
-      </div>
+    <div>
+      <TimeComponent />
     </div>
   )
 }
