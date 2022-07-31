5459e253f9a5549e14d0fda94800ed08f44a4b27
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2022-08-01 00:40:34 +0530

    feat: pause logic

--- src/components/TimeComponent/TimeComponent.tsx
+++ src/components/TimeComponent/TimeComponent.tsx
@@ -11,7 +11,11 @@ const TimerComponent: FunctionComponent<Props> = (props: Props) => {
   const { hours, minutes, seconds, flag } = props
   const Ref = useRef<any>(null)
 
-  const [timer, setTimer] = useState<any>('00:00:00')
+  const [timer, setTimer] = useState<string>('00:00:00')
+  const [formatSec, setFormatSec] = useState<string | number>('00')
+  const [formatMin, setFormatMin] = useState<string | number>('00')
+  const [formatHour, setFormatHour] = useState<string | number>('00')
+  const [pause, setPause] = useState<boolean>(false)
 
   const getTimeRemaining = (e: any) => {
     const total: any = Date.parse(e) - Date.parse(new Date().toString())
@@ -40,27 +44,55 @@ const TimerComponent: FunctionComponent<Props> = (props: Props) => {
           (secondsT > 9 ? secondsT : '0' + secondsT)
       )
     }
+    setFormatHour(hoursT > 9 ? hoursT : '0' + hoursT)
+    setFormatMin(minutesT > 9 ? minutesT : '0' + minutesT)
+    setFormatSec(secondsT < 9 ? '0' + secondsT : secondsT.toString())
   }
 
   const clearTimer = (e: any) => {
     // If you adjust it you should also need to
     // adjust the Endtime formula we are about
     // to code next
+
     setTimer(
       `${hours < 9 ? '0' + hours : hours}:${
         minutes < 9 ? '0' + minutes : minutes
       }:${seconds < 9 ? '0' + seconds : seconds}`
     )
+    setFormatHour(hours < 9 ? '0' + hours : hours)
+    setFormatMin(minutes < 9 ? '0' + minutes : minutes)
+    setFormatSec(seconds < 9 ? '0' + seconds : seconds)
 
     // If you try to remove this line the
     // updating of timer Variable will be
     // after 1000ms or 1sec
-    if (Ref.current) clearInterval(Ref.current)
+
+    if (Ref.current || pause) clearInterval(Ref.current)
+
     const id = setInterval(() => {
       startTimer(e)
     }, 1000)
     Ref.current = id
   }
+  useEffect(() => {
+    if (pause) clearInterval(Ref.current)
+    else {
+      let [hours, minutes, seconds] = timer.split(':')
+      let deadline = new Date()
+
+      // This is where you need to adjust if
+      // you entend to add more time
+      deadline.setHours(deadline.getHours() + parseInt(hours))
+      deadline.setMinutes(deadline.getMinutes() + parseInt(minutes))
+      deadline.setSeconds(deadline.getSeconds() + parseInt(seconds))
+      setTimer(
+        `${parseInt(hours) < 9 ? '0' + hours : hours}:${
+          parseInt(minutes) < 9 ? '0' + minutes : minutes
+        }:${parseInt(seconds) < 9 ? '0' + seconds : seconds}`
+      )
+      clearTimer(deadline)
+    }
+  }, [pause])
 
   const getDeadTime = () => {
     let deadline = new Date()
@@ -81,20 +113,34 @@ const TimerComponent: FunctionComponent<Props> = (props: Props) => {
     clearTimer(getDeadTime())
   }
 
+  const onClickPause = () => {
+    setPause(!pause)
+  }
+
   return (
     <>
-      <div className="flex flex-wrap justify-center p-10 gap-10 grow">
-        <div>
-          <h2>{timer}</h2>
+      <div className="flex flex-wrap justify-center py-10 gap-5 grow">
+        <div className="text-slate-700 text-6xl lg:text-9xl font-semibold">
+          {timer}
         </div>
       </div>
       <div className="flex flex-wrap justify-center  gap-10 grow">
-        <button
-          className="px-12 py-2 tracking-wider rounded-lg shadow-lg bg-slate-400 hover:ring-2 hover:bg-slate-500 hover:text-slate-100 ring-slate-400 ring-offset-2"
-          onClick={onClickReset}
-        >
-          Reset
-        </button>
+        <div>
+          <button
+            className="px-12 py-2 tracking-wider rounded-lg shadow-lg bg-slate-400 hover:ring-2 hover:bg-slate-500 hover:text-slate-100 ring-slate-400 ring-offset-2"
+            onClick={onClickReset}
+          >
+            Reset
+          </button>
+        </div>
+        <div>
+          <button
+            className="px-12 py-2 tracking-wider rounded-lg shadow-lg bg-slate-400 hover:ring-2 hover:bg-slate-500 hover:text-slate-100 ring-slate-400 ring-offset-2"
+            onClick={() => onClickPause()}
+          >
+            Pause
+          </button>
+        </div>
       </div>
     </>
   )
