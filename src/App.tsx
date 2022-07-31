131cc0c4625e128c6f11ce2c0167da5256196e14
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2022-07-31 16:32:45 +0530

    feat: initial Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import AppBar from './components/AppBar'
+import BreakComponent from './components/BreakComponent/BreakComponent'
+import Footer from './components/Footer'
 import TimeComponent from './components/TimeComponent'
 
 function App() {
   return (
-    <div>
+    <div className="mx-28 h-screen">
       <AppBar />
+      <BreakComponent />
       <TimeComponent />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from 'react'
+
+const Footer: FunctionComponent = () => {
+  return (
+    <div className="flex flex-wrap justify-center text-slate-500 pt-60 ">
+      <div>© 2022 Ananya Dhananjaya. All Rights Reserved.</div>
+    </div>
+  )
+}
+
+export default Footer
