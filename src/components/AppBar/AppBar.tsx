c8553d9aedc49406f4623e9b59f89be565363bbb
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2022-07-31 16:33:43 +0530

    chore: styling

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -2,8 +2,8 @@ import { FunctionComponent } from 'react'
 
 const AppBar: FunctionComponent = () => {
   return (
-    <div className="divide-y mx-14">
-      <div className="flex flex-row justify-between mx-14 p-6 gap-10">
+    <div className="divide-y ">
+      <div className="flex flex-row flex-wrap justify-between p-6 gap-10">
         <div>Pomodora</div>
         <div className="flex gap-8">
           <div>Settings</div>
